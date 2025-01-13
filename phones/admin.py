a1ba44003d37331e5d0ec80ae4fd955cd7b7a57c
Uelsky <maktavish123@gmail.com>
2025-01-13 12:20:31 +0300

    Initial commit

---
+++ phones/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Phone
+
+@admin.register(Phone)
+class PhoneAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'name', 'image',
+        'price', 'release_date',
+        'lte_exists', 'slug'
+    ]
